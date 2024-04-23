b57e2f3b9d6e03c1fe47f9fe63a4c45472d4a780
Yenting Chen <yeonbo0620@gmail.com>
2024-04-23 18:00:13 +0100

    Initial Commit

---
+++ JLPT N4 Grammar/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  JLPT N4 Grammar
+//
+//  Created by Yenting Chen on 23/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JLPT N4 Grammar/JLPT_N4_GrammarApp.swift
@@ -0,0 +1,17 @@
+//
+//  JLPT_N4_GrammarApp.swift
+//  JLPT N4 Grammar
+//
+//  Created by Yenting Chen on 23/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct JLPT_N4_GrammarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
